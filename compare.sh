76b07b4265ef504e2fc7cc6f447225adca310609
Alex Rudy <github@alexrudy.net>
2023-02-23 16:29:17 +0000

    Remove unneded dev script

--- compare.sh
+++
@@ -1,1 +0,0 @@
-openssl asn1parse -in yacme-key/test-examples/csr/example.csr | awk '{$1=$2=$3=$4=""; print $0}' | diff - <(openssl asn1parse -in yacme-key/test-examples/csr/example-openssl.csr | awk '{$1=$2=$3=$4=""; print $0}')
